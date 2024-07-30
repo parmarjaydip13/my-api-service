748c71c053cc7ced7f14064127e24d9bee4d10df
vipul kava <vipul.kava@p2dl.com>
2024-07-30 18:10:08 +0530

    result extension mapper

--- src/TodoApplication.API/Extensions/ResultExtensions.cs
+++ src/TodoApplication.API/Extensions/ResultExtensions.cs
@@ -1,21 +1,46 @@
-﻿using SharedKernel;
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedKernel;
 
 namespace TodoApplication.API.Extensions
 {
+    
     public static class ResultExtensions
     {
-        public static IResult ToProblemDetails(this Result result)
+        public static IResult ToProblemResult(this Result result)
         {
             if (result.IsSuccess)
             {
                 throw new InvalidOperationException();
             }
 
-            //return Results.Problem(
-            //    statusCode: ,
-            //    title: result.Error.ErrorType.ToString(),
-                
-            //    );
+            var problemDetails = new ProblemDetails
+            {
+                Type = ,
+                Status = GetStatusCode(result.Error.ErrorType),
+                Title = GetTitle(result.Error.ErrorType),
+                Instance = "",
+                Detail = result.Error.Description
+            };
+
+            return Results.Problem(problemDetails);
         }
+
+        private static int GetStatusCode(ErrorType errorType) =>
+            errorType switch
+            {
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                _ => StatusCodes.Status500InternalServerError // Fallback for unknown error types
+            };
+
+        private static string GetTitle(ErrorType errorType) =>
+            errorType switch
+            {
+                ErrorType.Validation => "Bad Request",
+                ErrorType.NotFound => "Not Found",
+                ErrorType.Conflict => "Conflict",
+                _ => "Unexpected Error" // Fallback for unknown error types
+            };
     }
 }
